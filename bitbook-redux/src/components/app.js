e6aefd92cc6fd37e811e7dcf061930976991fa47
Nenad Radovanovic <nesaplay@gmail.com>
2017-12-01 09:51:08 +0100

    redux

---
+++ bitbook-redux/src/components/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import HelloWorld from "./helloWorld/helloWorld";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <HelloWorld />;
+    }
+}
+
+export default App;
