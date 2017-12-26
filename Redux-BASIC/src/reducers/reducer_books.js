2b9c627678e7825e35d2cc7e3a573cb81c569619
Nenad Radovanovic <nesaplay@gmail.com>
2017-12-26 10:47:14 +0100

    new HOC

--- Redux-BASIC/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-    return [
-        { title: 'Javascript: The good parts', pages: 101},
-        { title: 'Blabla', pages: 55},
-        { title: 'Test', pages: 215},
-        { title: 'Demo', pages: 12},
-    ]
-}
